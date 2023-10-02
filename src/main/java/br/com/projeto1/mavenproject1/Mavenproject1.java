70179b87cee59a92bfd211a935ab5ed44c0928f4
Leonardo Melo <leonardocicco.9159@aluno.saojudas.br>
2023-10-02 20:58:51 -0300

    ap11(login): campos login e senha

---
+++ src/main/java/br/com/projeto1/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.projeto1.mavenproject1;
+
+/**
+ *
+ * @author 820152408
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
